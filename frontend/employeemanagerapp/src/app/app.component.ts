5cc80529b8f7674c43522facafdd500aced47e2e
samodpriyankara <samodpriyankara20562@gmail.com>
2025-01-24 11:29:10 +0530

    front end

---
+++ frontend/employeemanagerapp/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  public employees: Employee[] | undefined;
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit(){
+    this.getEmployees;
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployee().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+
+      }
+    );
+
+  }
+}
